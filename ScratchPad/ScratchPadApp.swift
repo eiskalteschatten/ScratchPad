f33f5781dd6eaca6d63e9c4571789c7350d4c9af
Alex Seifert <alex@alexseifert.com>
2022-12-09 10:55:11 +0100

    Initial Commit

---
+++ ScratchPad/ScratchPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchPadApp.swift
+//  ScratchPad
+//
+//  Created by Alex Seifert on 09.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ScratchPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
