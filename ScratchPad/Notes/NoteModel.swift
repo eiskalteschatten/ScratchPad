c082aa5a58d75d61384ec0b1b6084109248da969
Alex Seifert <alex@alexseifert.com>
2022-12-09 17:59:02 +0100

    Start trying to get permission to access user-selected folders

--- ScratchPad/Notes/NoteModel.swift
+++ ScratchPad/Notes/NoteModel.swift
@@ -14,8 +14,7 @@ final class NoteModel: ObservableObject {
     @Published var pageNumber = UserDefaults.standard.value(forKey: "pageNumber") as? Int ?? 1 {
         didSet {
             UserDefaults.standard.set(pageNumber, forKey: "pageNumber")
-            
-            // TODO: load note contents and set noteContents
+            openNote()
         }
     }
     
@@ -29,14 +28,12 @@ final class NoteModel: ObservableObject {
     init() {
         noteContents = NSAttributedString(string: "")
         
-//        $noteContents
-//            .debounce(for: .seconds(1), scheduler: DispatchQueue.main)
-//            .sink { self.saveNote($0) }
-//            .store(in: &noteContentsBag)
+        $noteContents
+            .debounce(for: .seconds(1), scheduler: DispatchQueue.main)
+            .sink { self.saveNote($0) }
+            .store(in: &noteContentsBag)
         
         openNote()
-        
-//        createStorageLocation()
     }
     
     private func openNote() {
@@ -50,40 +47,34 @@ final class NoteModel: ObservableObject {
         let options = [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.rtfd]
         
         do {
-            let attributedString = try NSAttributedString(url: fullURL, options: options, documentAttributes: nil)
-            noteContents = attributedString
+            if fullURL.startAccessingSecurityScopedResource() {
+                let attributedString = try NSAttributedString(url: fullURL, options: options, documentAttributes: nil)
+                noteContents = attributedString
+                fullURL.stopAccessingSecurityScopedResource()
+            }
+        } catch {
+            // TODO: do something here. This is also a bad error!
+            print(error)
+        }
+    }
+    
+    private func saveNote(_ contents: NSAttributedString) {
+        guard let storageLocation = UserDefaults.standard.url(forKey: "storageLocation") else {
+            // TODO: do something. This is a bad error!
+            print("error1")
+            return
+        }
+        
+        let fullURL = storageLocation.appendingPathComponent(noteName)
+
+        do {
+            if fullURL.startAccessingSecurityScopedResource() {
+                try contents.rtfd().write(to: fullURL)
+                fullURL.stopAccessingSecurityScopedResource()
+            }
         } catch {
             // TODO: do something here. This is also a bad error!
             print(error.localizedDescription)
         }
     }
-    
-//    private func saveNote(_ contents: NSAttributedString) {
-//        guard let storageLocation = UserDefaults.standard.url(forKey: "storageLocation") else {
-//            // TODO: do something. This is a bad error!
-//            print("error1")
-//            return
-//        }
-//
-//        let fullURL = storageLocation.appendingPathComponent(noteName)
-//
-//        do {
-//            try contents.rtfd().write(to: fullURL)
-//        } catch {
-//            // TODO: do something here. This is also a bad error!
-//            print(error.localizedDescription)
-//        }
-//    }
-    
-//    private func createStorageLocation() {
-//        if
-//            let storageLocation = UserDefaults.standard.url(forKey: "storageLocation"),
-//            !FileManager.default.fileExists(atPath: storageLocation.absoluteString) {
-//            do {
-//                try FileManager.default.createDirectory(atPath: storageLocation.absoluteString, withIntermediateDirectories: true, attributes: nil)
-//            } catch {
-//                print(error.localizedDescription);
-//            }
-//        }
-//    }
 }
