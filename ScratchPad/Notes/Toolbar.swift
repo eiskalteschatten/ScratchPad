c9d87245fe516752d209bda449d0d20e51a2ef51
Alex Seifert <alex@alexseifert.com>
2026-02-20 16:45:03 +0100

    Update the toolbar positioning

--- ScratchPad/Notes/Toolbar.swift
+++ ScratchPad/Notes/Toolbar.swift
@@ -7,43 +7,47 @@
 
 import SwiftUI
 
-struct Toolbar: View {
+struct Toolbar: ToolbarContent {
     @EnvironmentObject var noteModel: NoteModel
     
     @FocusState private var pageNumberTextfieldIsFocused: Bool
     
-    var body: some View {
-        Button(action: {
-            noteModel.appendNewNote()
-        }) {
-            Label("New Page", systemImage: "plus")
+    var body: some ToolbarContent {
+        ToolbarItemGroup(placement: .primaryAction) {
+            Button(action: {
+                noteModel.appendNewNote()
+            }) {
+                Label("New Page", systemImage: "plus")
+            }
         }
         
-        Button(action: {
-            if noteModel.pageNumber > 1 {
-                noteModel.pageNumber -= 1
+        ToolbarItemGroup(placement: .navigation) {
+            Button(action: {
+                if noteModel.pageNumber > 1 {
+                    noteModel.pageNumber -= 1
+                }
+                
+                unfocusPageNumberTextfield()
+            }) {
+                Label("Back", systemImage: "chevron.left")
+            }
+            .disabled(noteModel.pageNumber <= 1)
+            
+            Button(action: {
+                noteModel.pageNumber += 1
+                unfocusPageNumberTextfield()
+            }) {
+                Label("Forward", systemImage: "chevron.right")
             }
             
-            unfocusPageNumberTextfield()
-        }) {
-            Label("Back", systemImage: "chevron.left")
+            TextField("", value: Binding<Int>(
+                get: { noteModel.pageNumber },
+                set: { noteModel.pageNumber = abs($0) }
+            ), formatter: NumberFormatter())
+            .multilineTextAlignment(.center)
+            .focused($pageNumberTextfieldIsFocused)
+            .frame(width: 50)
         }
-        .disabled(noteModel.pageNumber <= 1)
-        
-        Button(action: {
-            noteModel.pageNumber += 1
-            unfocusPageNumberTextfield()
-        }) {
-            Label("Forward", systemImage: "chevron.right")
-        }
-        
-        TextField("", value: Binding<Int>(
-            get: { noteModel.pageNumber },
-            set: { noteModel.pageNumber = abs($0) }
-        ), formatter: NumberFormatter())
-        .multilineTextAlignment(.center)
-        .focused($pageNumberTextfieldIsFocused)
-        .frame(width: 50)
     }
     
     private func unfocusPageNumberTextfield() {
@@ -53,6 +57,9 @@ struct Toolbar: View {
 
 struct Toolbar_Previews: PreviewProvider {
     static var previews: some View {
-        Toolbar()
+        Text("Preview")
+            .toolbar {
+                Toolbar()
+            }
     }
 }
