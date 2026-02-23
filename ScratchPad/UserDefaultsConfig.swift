7f7cd13c4760a08079bc65545308cbdb5e48d035
Alex Seifert <alex@alexseifert.com>
2026-02-23 10:07:17 +0100

    The variables are constants

--- ScratchPad/UserDefaultsConfig.swift
+++ ScratchPad/UserDefaultsConfig.swift
@@ -7,16 +7,16 @@
 
 final class UserDefaultsConfig {
     #if DEBUG
-    static var storageLocationBookmarkData = "storageLocationBookmarkData-debug"
-    static var windowTransparency = "windowTransparency-debug"
-    static var floatAboveOtherWindows = "floatAboveOtherWindows-debug"
-    static var openWelcomeSheetOnLaunch = "openWelcomeSheetOnLaunch-debug"
-    static var pageNumber = "pageNumber-debug"
+    static let storageLocationBookmarkData = "storageLocationBookmarkData-debug"
+    static let windowTransparency = "windowTransparency-debug"
+    static let floatAboveOtherWindows = "floatAboveOtherWindows-debug"
+    static let openWelcomeSheetOnLaunch = "openWelcomeSheetOnLaunch-debug"
+    static let pageNumber = "pageNumber-debug"
     #else
-    static var storageLocationBookmarkData = "storageLocationBookmarkData"
-    static var windowTransparency = "windowTransparency"
-    static var floatAboveOtherWindows = "floatAboveOtherWindows"
-    static var openWelcomeSheetOnLaunch = "openWelcomeSheetOnLaunch-debug"
-    static var pageNumber = "pageNumber"
+    static let storageLocationBookmarkData = "storageLocationBookmarkData"
+    static let windowTransparency = "windowTransparency"
+    static let floatAboveOtherWindows = "floatAboveOtherWindows"
+    static let openWelcomeSheetOnLaunch = "openWelcomeSheetOnLaunch-debug"
+    static let pageNumber = "pageNumber"
     #endif
 }
